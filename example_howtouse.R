10b0454d313c8e9697cf666b7cba6ae0fff5eb88
MartaRotari <36338658+MartaRotari@users.noreply.github.com>
2022-09-16 15:47:57 +0200

    Example fix
    Fixed the Example and How to use file

--- example_howtouse.R
+++ example_howtouse.R
@@ -1,53 +1,45 @@
 library(MASS)
 library(corrplot)
 library(Matrix)
-#########################################################
 
-#########################################################
 
-correlation <- 0.7
-n_data <- 150
-
-######################################################### 
-#####################   DATA
-    
+#### Simulate some data: X=[X1,..,X20] and Y=beta1*X2 + beta2*X7
+n_data <- 200
 correlation_matrix <- matrix(0,20,20)
-correlation_matrix[1:5,1:5] <- correlation
-correlation_matrix[10:14,10:14] <- correlation
-diag(correlation_matrix)<-1
+correlation_matrix[1:5,1:5] <- 0.7
+diag(correlation_matrix) <- 1
 data = data.frame( mvrnorm( n = n_data, mu = rep(0,20), Sigma = correlation_matrix ) )
-corrplot( cor(data) )
-var_noise <- 0.1
+var_noise = 0.1
 noise = rnorm( n_data , mean = 0, sd = sqrt(var_noise) )
-X = cbind( data$X2, data$X11, data$X19, data$X20)
+X = cbind( data$X2, data$X7)
 var_beta = var_noise * solve( t(X)%*%X )
 sd_beta = sqrt( diag(var_beta) )
-ratio = c(4,3,3,5)
+ratio = 3  # The simulated data are such that beta/sd(beta) > ratio, where ratio > 2
 beta = sd_beta*ratio
-data$y = beta[1]*data$X2 + beta[2]*data$X11 + beta[3]*data$X19 + beta[4]*data$X20 + noise
+data$y = beta[1]*data$X2 + beta[2]*data$X7 + noise
 
-######################################################### 
-#####################   Run Boruta Conditional    
+####  Run Boruta Conditional    
 source("boruta_conditional.R")
 
 cb <- Condition_Boruta(y~., data = data, doTrace = 1, getImp=getImpRfCond)
 print(cb)
 
+# Fix the unclassified variables
 final.boruta <- TentativeRoughFix(cb)
 print(final.boruta)
-
-#List of confirmed attributes
+# List of confirmed attributes
 getSelectedAttributes(final.boruta, withTentative = F)
+# Historical mean of the variables importance value
 plotImpHistory(final.boruta)
-
-## Dataframe of result
+# Dataframe of result
 boruta.df <- attStats(cb)
 print(boruta.df)
-
 # plot the Importance box plot
 plot.Boruta(cb)   
+plot.Boruta(final.boruta)
 
+#### Original Boruta algorithm
+library(Boruta)
 
-
-
-
+b_model = Boruta(y~., data = data, doTrace = 1)
+print(b_model)
